8336869035d9fd546baae2d2bbc7c471c7c0f0b6
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-12 22:13:46 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,1 +1,9 @@
-
+#Using native Python programming to calculate measure of central tendency
+data=[i for i in range(2000)]
+count=len(data)
+summation= sum(data)
+mean= sum(data)/len(data)
+mode=max(set(data),key=data.count)
+mid_value= len(data)//2
+median= (data[mid]+data[~mid])/2
+print('The sequence has {} numbers, the sum of all number is {}, the mean is {}, the mode is {}, and the median is {}'.format(count,summation,mean,mode,median))
