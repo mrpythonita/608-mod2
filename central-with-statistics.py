b55e554c9740184f6aa1efafd265b1a876ce6f0d
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-12 22:27:17 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,4 @@
 
+import statistics
+data=[i for i in range(1,2000)]
+print('The sequence "data" has {} numbers, the sum of its numbers is {}, the mean is {}, the median is {}, and the mode is {}.'.format(len(data),sum(data),statistics.mean(data),statistics.median(data), statistics.mode(data)))
