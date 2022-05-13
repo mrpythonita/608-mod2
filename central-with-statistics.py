d4cbd760a79a59d059259f0c1e8556f6717c513b
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-13 16:14:14 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,6 +1,5 @@
 # calculating measure of central tendency with the module statistics.
 import statistics
 data=[i for i in range(1,2000)]
-print('The sequence "data" has {} numbers, the sum of its numbers is {}, the mean is {}, the median is {}, and the mode is {}.'.format(\
-    len(data),sum(data),statistics.mean(data),statistics.median(data),\
-    statistics.mode(data)))
+print('The sequence "data" has {} numbers, the sum of all numbers is {}, the mean is {}, the median is {}, and the mode is {}.'.format(\
+    len(data),sum(data),statistics.mean(data),statistics.median(data), statistics.mode(data)))
