7b405ede191b3505fdfc6b3915785d6b3c2c9f34
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-13 14:50:55 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -5,5 +5,5 @@ summation= sum(data)
 mean= sum(data)/len(data)
 mode=max(set(data),key=data.count)
 mid_value= len(data)//2
-median= (data[mid]+data[~mid])/2
+median= (data[mid_value]+data[~mid_value])/2
 print('The sequence has {} numbers, the sum of all number is {}, the mean is {}, the mode is {}, and the median is {}'.format(count,summation,mean,mode,median))
